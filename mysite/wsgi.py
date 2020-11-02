1ca4131cb839f916997c13125ca228bed32192b4
hirondelle4 <merciereva93@gmail.com>
2020-11-02 21:17:21 +0100

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-project_folder = os.path.expanduser('~/tuto2')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
