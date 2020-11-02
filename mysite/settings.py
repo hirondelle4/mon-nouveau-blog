07913514b1cd342148344ac9ff9b35a6a2bc54b1
hirondelle4 <merciereva93@gmail.com>
2020-11-02 21:09:21 +0100

    my site changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 #SECRET_KEY = os.environ['SECRET_KEY']
 
 #with open('~/etc/secret_key.txt', 'r') as f:
@@ -86,7 +86,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
